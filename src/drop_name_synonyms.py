b42c20f382c11db3e1ef87405b2d610dc928a1ee
Michael Halle <m -@- halle.us>
2018-01-02 16:40:29 -0500

    remove name_en and name_la from synonyms having names repeated in the synonyms was shortsighted

---
+++ src/drop_name_synonyms.py
@@ -0,0 +1,13 @@
+import sqlite3
+import sys
+
+
+def drop_name_synonyms(dbfilename):
+    with sqlite3.connect(dbfilename) as conn:
+        cur = conn.cursor()
+        cur.execute("""delete from synonyms 
+            where synonym_type in ('name_en', 'name_la');""")
+
+
+if __name__ == '__main__':
+    drop_name_synonyms(sys.argv[1])
